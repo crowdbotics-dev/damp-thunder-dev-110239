a80a241a5c5e23dd7724a185781767c2d914f3f1
Crowdbotics <engineering@crowdbotics.com>
2023-08-22 00:52:10 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "damp-thunder-dev-110239",
+  url: "https://damp-thunder-dev-110239.botics.co",
+  api: "https://damp-thunder-dev-110239.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
